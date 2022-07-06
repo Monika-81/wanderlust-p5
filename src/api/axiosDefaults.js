d269bce4884bf7dbf10336d04b7aff407c8da52d
Monika Jakobsson <nimrin81@gmail.com>
2022-07-06 19:19:10 +0000

    Try error fix for development after pushing code to heroku; 'CORS policy: No 'Access-Control-Allow-Origin' header'.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,6 +3,8 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://wanderlust-api-p5.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+// try while developing, getting CORS error:
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+export const axiosReq = axios.create()
+export const axiosRes = axios.create()
